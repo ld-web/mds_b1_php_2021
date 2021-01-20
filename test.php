8b80150a85522aab0069f62eabc674aa1c72df98
Lucas Delobelle <lucas@ld-web.net>
2021-01-20 11:49:38 +0100

    Tableaux et boucle for

--- test.php
+++ test.php
@@ -18,6 +18,7 @@
   $numero = 1;
   $uneVariableBooleenne = true; // booléen : soit true, soit false
   $uneVariableFlottante = 198.6;
+  $monPrenom = "Lucas";
   // Utilisation de guillemets doubles : possibilité d'injecter la variable
   echo "Numéro : $numero<br />";
   // Guillemets simples : la variable n'est pas interprétée
@@ -25,9 +26,38 @@
   // Opérateur de concaténation
   echo 'Numéro : ' . $numero . '<br />';
 
+  // Déclaration de constante : sans le '$'
   const MA_CONSTANTE = 123;
   echo MA_CONSTANTE;
+
+  // Déclaration d'un tableau contenant 5 valeurs entières
+  $monTableau = [1, 2, 3, 4, 5];
+  // Indices :        0, 1   , 2
+  $monAutreTableau = [6, true, $monPrenom];
+  var_dump($monAutreTableau);
+  // Accès à une valeur par un index numérique
+  echo $monAutreTableau[0];
+
+  $utilisateur = [
+    0 => "Coucou",
+    'login' => 'Lucas',
+    'email' => 'test@test.com',
+    'account_activated' => false
+  ];
+  var_dump($utilisateur);
+  // Accès à une valeur par une clé nommée
+  echo $utilisateur['email'];
+  var_dump($utilisateur[0]);
   ?>
+  <p>
+    <?php
+    // Boucle for :
+    // Instruction d'initialisation; condition de maintien; instruction de pas
+    for ($i = 0; $i < 10; $i++) {
+      echo $i . "<br />";
+    }
+    ?>
+  </p>
 </body>
 
 </html>
