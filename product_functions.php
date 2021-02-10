b031d1a9517145dd547e1b23dd8fd29f83665a5b
Lucas Delobelle <lucas@ld-web.net>
2021-02-10 15:49:52 +0100

    getPriceTtc : add rate parameter

--- product_functions.php
+++ product_functions.php
@@ -1,12 +1,13 @@
 <?php
 
 /**
- * Returns full price from a given price with VAT
+ * Returns full price from a given price without VAT
  *
  * @param float $price Price without VAT
+ * @param float $rate VAT rate (0 < rate < 1)
  * @return float Price with VAT
  */
-function getPriceTtc(float $price): float
+function getPriceTtc(float $price, float $rate = 0.2): float
 {
-  return $price * 1.2;
+  return $price * (1 + $rate);
 }
