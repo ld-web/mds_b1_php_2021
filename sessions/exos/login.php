d22be0962b96b6d789a5955f64b7bb617ce3bbb4
Lucas Delobelle <lucas@ld-web.net>
2021-03-24 10:29:41 +0100

    Connexion utilisateur avec BDD (PDO, prepared statement)

--- sessions/exos/login.php
+++ sessions/exos/login.php
@@ -8,21 +8,53 @@ $errorMessage = '';
 if (!empty($_POST)) {
   // Si l'utilisateur a saisi un login ET un mot de passe
   if (!empty($_POST['login']) && !empty($_POST['password'])) {
-    if ($_POST['login'] !== $login) {
-      $errorMessage = 'Invalid login';
-    } elseif ($_POST['password'] !== $password) {
-      $errorMessage = 'Invalid password';
+    $dsn = "mysql:dbname=mds_b1_php;host=127.0.0.1;charset=utf8mb4";
+    $pdo = new PDO($dsn, "mds_b1_php", "FUbaMxoyMJFuuWxp");
+
+    $login = $_POST['login']; // email
+    $password = $_POST['password'];
+
+    // REQUÊTE NON PREPAREE = DANGEREUX
+    // Je construis ma requête dans une chaîne de caractères
+    // $query = "SELECT * FROM client WHERE email='$login' AND pass='$password'";
+    // // Je demande à PDO d'exécuter cette requête
+    // $stmt = $pdo->query($query);
+
+    // REQUÊTE PREPAREE
+    $query = 'SELECT * FROM client WHERE email=:username AND pass=:pass';
+    $stmt = $pdo->prepare($query);
+    $stmt->execute([
+      'username' => $login,
+      'pass' => $password
+    ]);
+
+    // fetch me renverra les résultats un par un
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    // Si fetch renvoie "false", alors je n'ai aucun utilisateur correspondant au login et au mot de passe entrés dans le formulaire
+    if ($user === false) {
+      $errorMessage = 'Invalid login or password';
     } else {
-      // On manipule notre session en partant du principe que notre header.php
-      // s'occupe du session_start().
-      // Ce n'est pas forcément toujours le cas, ainsi :
-      // - Il peut être utile, avant de manipuler la session, de s'assurer qu'elle est bien démarrée
-      // - Pour ce faire, vous pouvez utiliser la méthode session_status de la SPL :
-      // https://www.php.net/manual/en/function.session-status
       $_SESSION['login'] = $login;
       header('Location: admin.php');
       exit;
     }
+
+    // if ($_POST['login'] !== $login) {
+    //   $errorMessage = 'Invalid login';
+    // } elseif ($_POST['password'] !== $password) {
+    //   $errorMessage = 'Invalid password';
+    // } else {
+    //   // On manipule notre session en partant du principe que notre header.php
+    //   // s'occupe du session_start().
+    //   // Ce n'est pas forcément toujours le cas, ainsi :
+    //   // - Il peut être utile, avant de manipuler la session, de s'assurer qu'elle est bien démarrée
+    //   // - Pour ce faire, vous pouvez utiliser la méthode session_status de la SPL :
+    //   // https://www.php.net/manual/en/function.session-status
+    //   $_SESSION['login'] = $login;
+    //   header('Location: admin.php');
+    //   exit;
+    // }
   } else {
     $errorMessage = 'All fields are required';
   }
